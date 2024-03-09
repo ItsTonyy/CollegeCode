ef4ce21926b7b418335185999e98482694f2e2d4
Tony <112224948+ItsTonyy@users.noreply.github.com>
2024-03-09 15:49:28 -0300

    code finished

---
+++ DesignOfComputationalAlgorithms/CheckDateProgram/CheckDate.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int anoBissexto(int ano) {
+    return (ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0));
+}
+
+int main() {
+    int dia, mes, ano;
+    printf("Bem-vindo ao Verificador de Data!\n\n");
+    printf("Por favor insira a data que voce deseja verificar no formato 'dia mes ano' : ");
+    scanf("%d%d%d", &dia, &mes, &ano);
+
+    // checar se a data é invalida
+    if ((dia < 1 || dia > 31) || (mes < 1 || mes > 12) || (ano < 1)) {
+        printf("Erro: A data inserida e invalida. Certifique-se de inserir uma data existente.\n");
+        return 1;
+    }
+
+    // checar se a quantidade de dias em determinado mes está correta
+    if (mes == 2) {
+        if (dia > 29 || (dia == 29 && !anoBissexto(ano))) {
+            printf("Erro: Fevereiro nao pode ter %d dias em %d.\n", dia, ano);
+            return 1;
+        }
+    } else if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia > 30) {
+            printf("Erro: O mes %d não pode ter mais de 30 dias.\n", mes);
+            return 1;
+    }
+
+    // caso nada esteja errado, retorno uma mensagem de ok
+    printf("A data inserida e valida!\n");
+
+    return 0;
+}
+
+// meses com 31 dias: 1, 3, 5, 7, 8, 10, 12
+// meses com 30 dias: 4, 6, 9, 11
+// meses com 28 ou 29 dias: 2
