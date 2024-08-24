d3c72d38fc8e9ec46e9d11d9a574142a74fb9ac5
Tony <tonytnportela@outlook.com>
2024-08-24 00:00:48 -0300

    new code in assembly

---
+++ Assembly/leituraDeInteiros.asm
@@ -0,0 +1,24 @@
+.data
+	saudacao: .asciiz "Olá. Por favor, forneça sua idade: "
+	saida: .asciiz "Sua idade é: "
+
+.text
+	# Imprimir uma string
+	li $v0, 4 
+	la $a0, saudacao
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	# valor fornecido está em t0
+	move $t0, $v0
+	
+	li $v0, 4 
+	la $a0, saida
+	syscall
+	
+	li $v0, 1
+	move $a0, $t0
+	syscall
+	
