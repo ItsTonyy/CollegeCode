55099892a8d07b90b65c0d8ffe2b7298858e877a
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2024-10-11 23:59:26 -0300

    new bubble

---
+++ DataStructure/sortingAlgorithms/bubble_sort.c
@@ -0,0 +1,48 @@
+// first way
+#include <stdio.h>
+
+void bubble(int v[], int qt) {
+  int last = qt - 1;
+
+  for (last; last >= 0; last--) {
+    int changed = 0;
+    for (int i = 0; i < last; i++) {
+      if (v[i] > v[i + 1]) {
+        changed = 1;
+        int aux = v[i];
+        v[i] = v[i + 1];
+        v[i + 1] = aux;
+      }
+    }
+    if (!changed) {
+      return;
+    }
+  }
+}
+
+// second way
+
+void betterBubble(int arr[], int qt) {
+  for(int i = 0; i < qt - 1; i++) {
+    for(int j = i + 1; j < qt; j++) {
+      if(arr[i] > arr[j]) {
+        int aux = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = aux;
+      }
+    }
+  }
+}
+
+int main() {
+  int v[] = {40, 70, 10, 50, 20, 30, 15};
+  betterBubble(v, 7);
+
+  for (int i = 0; i < 7; i++) {
+    printf("%d, ", v[i]);
+  }
+
+  printf("/n");
+
+  return 0;
+}
