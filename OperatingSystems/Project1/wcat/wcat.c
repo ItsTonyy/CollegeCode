78229d46e67e9a2c82d1c781d63e0077693cbafc
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-04-04 20:13:21 -0300

    feat: added wgrep

--- OperatingSystems/Project1/wcat/wcat.c
+++ OperatingSystems/Project1/wcat/wcat.c
@@ -3,24 +3,25 @@
 
 #define BUFFER_SIZE 4096
 
-//argc = número de argumentos passados para o programa na linha de comando (contando o próprio)
-//argv = array de strings que contém todos os argumentos passados (incluindo o próprio)
+//argc = número de argumentos passados para o programa na linha de comando (contando a própria função)
+//argv = array de strings que contém todos os argumentos passados (incluindo a própria função)
+
 int main(int argc, char *argv[]) {
   for (int i = 1; i < argc; i++) {
     char buffer[BUFFER_SIZE];
 
-    FILE *fp = fopen(argv[i], "r");
-
     if(argc == 1) {
-      exit(0);
+      return 1;
     }
+
+    FILE *fp = fopen(argv[i], "r");
 
     if(fp == NULL) {
       printf("wcat: cannot open file\n");
-      exit(1);
+      return 1;
     }
 
-    while(fgets(buffer, BUFFER_SIZE, fp)) {
+    while((buffer, BUFFER_SIZE, fp)) {
       printf("%s", buffer);
     }
 
---
+++ OperatingSystems/Project1/wgrep/wgrep.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void searchPattern(FILE *file, char *word) {
+  //getline ajustará automaticamente
+  char *line = NULL;
+  size_t bufferSize = 0; 
+
+  while(getline(&line, &bufferSize, file) != -1) {
+    char *stringFilter = strstr(line, word);
+
+    if(stringFilter != NULL) {
+      printf("%s\n", stringFilter);
+      return;
+    }
+  }
+  free(line);
+}
+
+int main(int argc, char *argv[]) {
+  for (int i = 1; i < argc - 1; i++) {
+   
+    if(argc == 1) {
+      printf("wgrep: searchterm [file ...]\n");
+      return 1;
+    }
+
+    FILE *fp = fopen(argv[i + 1], "r");
+
+    if(fp == NULL) {
+      printf("wgrep: cannot open file\n");
+      return 1;
+    }
+
+    searchPattern(fp, argv[1]);
+
+    fclose(fp);
+  }
+
+  return 0;
+}
