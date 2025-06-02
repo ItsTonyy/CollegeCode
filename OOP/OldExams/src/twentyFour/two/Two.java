227482c2ad36fee6b4007b6c5ae6a78e8dc07ab3
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-06-02 01:01:12 -0300

    half of question 2

--- OOP/OldExams/src/twentyFour/two/Two.java
+++ OOP/OldExams/src/twentyFour/two/Two.java
@@ -1,4 +1,22 @@
 package twentyFour.two;
 
+class Planta {
+  private String id;
+  private String nome;
+  private double tamanho;
+
+  public Planta(String id) {
+    this.id = id;
+  }
+
+  @Override
+  public String toString() {
+    return "ID: " + id + ", Nome: " + nome + ", Tamanho: " + tamanho + "cm";
+  }
+}
+
 public class Two {
+  public static void main(String[] args) {
+
+  }
 }
