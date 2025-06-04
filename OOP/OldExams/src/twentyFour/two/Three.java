e13183a220f8bbbef1e0c361e2b62166d8eefaaf
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-06-04 15:56:21 -0300

    question 3

---
+++ OOP/OldExams/src/twentyFour/two/Three.java
@@ -0,0 +1,27 @@
+package twentyFour.two;
+
+public class Three {
+  // r1 = 1 instancia de rio
+  // origem de r1 = amazonia
+  // r2 = 2 instancia de rio
+  // r3 = 3 instancia de rio
+  // origem de r2 = mississipi
+  // r2 = r1, logo origem de r2 = amazonia
+  // origem de r2 = ganges
+  // origem de r1 = yangtzé
+  // printa na tela "yangtzé, ganges"
+  // x = 32
+  // volume de agua de r1 = 0
+  // printa na tela "32"
+  // x agora é 61
+  // volume de agua de r1 = 114
+  // origem de r1 = tamisa
+  // printa na tela "tamisa, 61, 114, 3"
+  // referência antiga de r2 sumiu, agora é um novo objeto
+  // origem de r2 é mekong
+  // total rios = 4
+  // printa na tela "mekong, 0"
+  // r3 recebe todos os dados de r2
+  // origem de r3 mudou pra danubio
+  // printa na tela "mekong"
+}
--- OOP/OldExams/src/twentyFour/two/Two.java
+++ OOP/OldExams/src/twentyFour/two/Two.java
@@ -1,5 +1,8 @@
 package twentyFour.two;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Planta {
   private String id;
   private String nome;
@@ -9,9 +12,59 @@ class Planta {
     this.id = id;
   }
 
+  public String getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public double getTamanho() {
+    return tamanho;
+  }
+
   @Override
   public String toString() {
     return "ID: " + id + ", Nome: " + nome + ", Tamanho: " + tamanho + "cm";
+  }
+}
+
+class Briofita extends Planta {
+  public Briofita(String id) {
+    super(id);
+  }
+
+  @Override
+  public String toString() {
+    return "[Briofita] " + super.toString();
+  }
+}
+
+class Pteridofita extends Planta {
+  public Pteridofita(String id) {
+    super(id);
+  }
+
+  @Override
+  public String toString() {
+    return "[Pteridofita] " + super.toString();
+  }
+}
+
+class Utils {
+  public static boolean existe(List lista, Planta planta) {
+    return lista.contains(planta);
+  }
+
+  public static Planta[] ordena(List lista) {
+    Planta[] plantasArray = lista.toArray(new Planta[0])
+
+    for (int i = 0; i < lista.size(); i++) {
+      for (int j = i + 1; j < lista.size(); j++) {
+
+      }
+    }
   }
 }
 
